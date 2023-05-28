a3d5f3b7804db51031888530c837f28f75e84a9d
mananatheone <aidanocmer04@outlook.com>
2023-05-28 15:03:34 +0800

    Some Changes

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int init() {
   if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
     std::cout << "SDL2 cannot be initialized.. Error:" << SDL_GetError() << "\n"; return -1; }
   window = SDL_CreateWindow("SomethingEngine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-    128, 96, 0
+    256, 192, 0
   ); if ( !window ) {
     std::cout << "Window cannot be created.. Error:" << SDL_GetError() << "\n"; return -1; 
   } winSurf = SDL_GetWindowSurface(window);
@@ -26,37 +26,25 @@ void quit() {
 
 int main(int argc, char* argv[]) {
   SDL_Event ev;
-  ISurface test_surf("nicetexture.bmp");
-  Spritesheet spritesheet(&test_surf);
 
-  SDL_Rect penasrect;
-    penasrect.x=penasrect.y=32;
-    penasrect.w=penasrect.h=32;
-  SDL_Surface* penas = spritesheet.fromRect(penasrect);
-
-  if ( init() ) { return -1; }
-
-  std::cout << "Test" << std::endl;
+  init();
 
   bool run = true;
-  while ( run ) {
-    while ( SDL_PollEvent(&ev) ) {
-      switch ( ev.type ) {
+  while (run) {
+    while (SDL_PollEvent(&ev)) {
+      switch (ev.type) {
         case SDL_QUIT:
           run = false;
+          break;
+        
+        default:
+          break;
       }
     }
-    // test_surf.SDLBlitTo(winSurf, 0, 0);
-    SDL_BlitSurface(penas, NULL, winSurf, NULL);
+    SDL_FillRect(winSurf, NULL, 0xffffffff);
     SDL_UpdateWindowSurface(window);
   }
-  // test_surf.~ISurface()
-  // No need for this ^ code right here.
 
-  // When using Spritesheet class' constructor you have to pass the pointer of the
-  // ISurface object meaning that when you call the deconstructor, it also deconstructs
-  // what you have passed in the Spritesheet ctor.
-  spritesheet.~Spritesheet();
   quit();
   return 0;
 }
